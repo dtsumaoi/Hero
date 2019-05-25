9c253391e5081d0eeb6da74e77d75bf731ba92ff
dtsumaoi <dtsumaoi@gmail.com>
2019-05-25 17:20:00 +0800

    initialized signin

--- app/src/main/java/com/example/hero/MainActivity.java
+++ app/src/main/java/com/example/hero/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.hero;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.hero.ui.login.LoginActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_signup)
+    Button btnSignup;
+
+    @BindView(R.id.txt_signin)
+    TextView btnSignin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/hero/SplashActivity.java
+++ app/src/main/java/com/example/hero/SplashActivity.java
@@ -1,29 +1,34 @@
 package com.example.hero;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class SplashActivity extends AppCompatActivity {
 
     private Handler mWaitHandler = new Handler();
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        mWaitHandler.postDelayed(new Runnable() {
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
 
+        mWaitHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
 
                 //The following code will execute after the 5 seconds.
-
                 try {
-
                     //Go to next page i.e, start the next activity.
                     Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                     startActivity(intent);
@@ -35,6 +40,14 @@ public class SplashActivity extends AppCompatActivity {
                 }
             }
         }, 3000);  // Give a 5 seconds delay.
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
     }
 
     @Override
